37f3c4b6a9c2a7de466a898721a985135e00f978
Timo Dittmann <timo.dittmann@mail.schwarz>
2025-06-18 20:39:10 +0200

    Fixed icons

--- vite.config.ts
+++ vite.config.ts
@@ -16,16 +16,16 @@ export default defineConfig({
         name: "Lovy",
         short_name: "Lovy",
         icons: [
-          {src: '/img/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png'},
-          {src: '/img/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png'},
+          {src: '/img/icons/192x192.png', sizes: '192x192', type: 'image/png'},
+          {src: '/img/icons/512x512.png', sizes: '512x512', type: 'image/png'},
           {
-            src: '/img/icons/android-chrome-maskable-192x192.png',
+            src: '/img/icons/192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: "maskable"
           },
           {
-            src: '/img/icons/android-chrome-maskable-512x512.png',
+            src: '/img/icons/512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: "maskable"
